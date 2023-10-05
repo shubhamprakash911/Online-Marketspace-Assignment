4c5c58afaee145ea2a3d9bfa1965148b28d79d64
Shubham Prakash <shubhamprakash9112000@gmail.com>
2023-10-05 11:02:14 +0530

    all user route created

--- controllers/userController.js
+++ controllers/userController.js
@@ -54,4 +54,55 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { loginUser, registerUser };
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({}).populate("listing", "cart", "product");
+  res.status(200).json(users);
+});
+
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+    .select("-password")
+    .populate("listing", "cart", "product");
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.address = req.body.address || user.address;
+    user.phone = req.body.phone || user.phone;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      address: updatedUser.address,
+      phone: updatedUser.phone,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+module.exports = { loginUser, registerUser, getUsers, getUserById, updateUser };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const { loginUser, registerUser } = require("../controllers/userController");
+const {
+  loginUser,
+  registerUser,
+  updateUser,
+  getUsers,
+  getUserById,
+} = require("../controllers/userController");
 
 const userRoute = express.Router();
 
 userRoute.post("/login", loginUser);
 userRoute.post("/register", registerUser);
+userRoute.put("/update", updateUser);
+userRoute.get("/", getUsers);
+userRoute.get("/:id", getUserById);
 
 module.exports = { userRoute };
